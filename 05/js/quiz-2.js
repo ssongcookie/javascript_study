4735eebe6d9754c412e4babcc5e2c14b1d7dd1bf
song <sgh83037@naver.com>
2022-04-21 09:49:14 +0900

    sumMultiQuiz

---
+++ 05/js/quiz-2.js
@@ -0,0 +1,13 @@
+var x = prompt();
+var y = prompt();
+sumMulti(x,y);
+
+function sumMulti(x,y){
+    
+    if(x==y){
+        alert(x+"와"+y+"는 같습니다.");
+    }else{
+        alert(x+"와"+y+"는 다릅니다.")
+    }
+}
+
