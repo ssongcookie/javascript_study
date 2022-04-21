0fa284f71999b8db8dea3448a59377f7e8aa3c22
song <sgh83037@naver.com>
2022-04-21 15:35:27 +0900

    기념일 계산

--- 06/js/console.js
+++ 06/js/console.js
@@ -44,3 +44,38 @@ function Book(author, pages, price, title){
 }
 
 jsBook = new Book("홍길동","500","15000","자바스크립트")
+
+//Date()객체 알아보기  (Date함수를 사용할 때 주의점: 결과값이 0부터 시작됨)
+
+// 현재 날짜 정보
+new Date()
+
+// 특정 날짜나 시간 정보
+new Date("1111-12-25");
+
+// 연도와 월까지만 지정했다면, 해당 월의 1일, 오전 9시로 설정됨
+new Date("1111-12");
+
+// 시간 정보까지 함께 지정하려면 날짜 다음에 대문자 T를 추가
+new Date("1111-12-25T18:00:00");
+
+// UTC(국제 표준시)로 지정하려면 맨 끝에 Z
+new Date("1111-12-25T18:00:00Z");
+
+
+// 자바스크립트의 날짜/시간 입력 방식
+// ISO 방식 (YYYY-MM-DDTHH:MM:SS)
+// 짧은 날짜 형식(MM/DD/YYYY)
+// 긴 날짜 형식(MM DD YYYY)
+
+//현재 날짜와 시간 정보 가져오기
+var now = new Date()
+//now 객체에 있는 정보 중 날짜 정보 가져오기
+now.getDate()
+//요일 정보 가져오기
+now.getDay()
+//시간 정보 가져오기
+now.getTime()
+
+//오늘 날짜로부터 50일 지난 후의 날짜를 계산
+now.setDate(now.getDate()+50)//오늘 날짜를 가져와 50을 더한 후 다시 날짜로 입력
